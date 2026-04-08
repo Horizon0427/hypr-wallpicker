5cadee237d1f8591e95a068a2b4e88611ee76918
GrandBIRDLizard <justin.munch9@gmail.com>
2026-04-08 15:35:48 -0400

    Add HasExtension function declaration to fs.h

--- include/fs.h
+++ include/fs.h
@@ -9,5 +9,6 @@ char *GetDefaultWallpaperDir(void);
 char *JoinPath(const char *dir, const char *name);
 char *BuildCacheImagePath(const char *cache_dir, const char *filename);
 bool IsSupportedWallpaperFile(const char *filename);
-
+bool HasExtension(const char *filename, const char *ext);
+  
 #endif
