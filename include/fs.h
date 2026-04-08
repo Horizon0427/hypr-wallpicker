3ae154b74e9f10d85aa8b23f0ef66b095bec664e
GrandBIRDLizard <justin.munch9@gmail.com>
2026-04-08 15:11:16 -0400

    Add new functions for wallpaper and path handling

--- include/fs.h
+++ include/fs.h
@@ -5,5 +5,9 @@
 #include <stddef.h>
 
 bool GetCacheDir(char *out_path, size_t max_len);
+char *GetDefaultWallpaperDir(void);
+char *JoinPath(const char *dir, const char *name);
+char *BuildCacheImagePath(const char *cache_dir, const char *filename);
+bool IsSupportedWallpaperFile(const char *filename);
 
 #endif
